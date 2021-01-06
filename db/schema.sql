e0e869f4790b077e5bb0ec774fed6587950d30bc
Kai Dong <dongkai981@gmail.com>
2021-01-06 17:13:22 -0600

    Adjust naming in schema.sql and completed seed.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,12 @@
 -- Drops the burger_db if it exists currently --
-DROP DATABASE IF EXISTS burger_db;
+DROP DATABASE IF EXISTS burgers_db;
 -- Creates the "burger_db" database --
-CREATE DATABASE burger_db;
+CREATE DATABASE burgers_db;
 
 -- Make it so all of the following code will affect burger_db --
-USE burger_db;
+USE burgers_db;
 -- Creates the table "favorite_foods" within burger_db --
-CREATE TABLE burger (
+CREATE TABLE burgers (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	burger_name varchar(255) NOT NULL,
 	devoured BOOLEAN DEFAULT false
---
+++ db/seed.sql
@@ -0,0 +1,7 @@
+INSERT INTO burgers (burger_name)
+VALUES ('Quater Pounder'),
+('Big Mac'),
+('Mushroom n Swiss');
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Parlour', 1);
