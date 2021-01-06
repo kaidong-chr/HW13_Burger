eac263140bc7c1f187585ecc17d73f2846f60ec5
Kai Dong <dongkai981@gmail.com>
2021-01-06 17:48:36 -0600

    Added selectAll method in orm.js

--- config/orm.js
+++ config/orm.js
@@ -1,3 +1,15 @@
 // Import mysql connection
 const connection = require("./connection");
 
+let orm = {
+    selectAll: function(tableInput, cb){
+        let queryString = "SELECT * FROM " + tableInput;
+        connection.query(queryString, function(err, res){
+            if (err) {
+                throw err;
+            }
+            cb(res);
+        });
+    },
+
+}
