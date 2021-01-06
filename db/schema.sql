f59891600b6b023096c10c0fc0bab058db04e8ef
Kai Dong <dongkai981@gmail.com>
2021-01-06 17:35:38 -0600

    Fixed some typos in schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,11 +1,11 @@
--- Drops the burger_db if it exists currently --
+-- Drops the burgers_db if it exists currently
 DROP DATABASE IF EXISTS burgers_db;
--- Creates the "burger_db" database --
+-- Creates the "burgers_db" database
 CREATE DATABASE burgers_db;
 
--- Make it so all of the following code will affect burger_db --
+-- Make it so all of the create table code will affect burgers_db
 USE burgers_db;
--- Creates the table "favorite_foods" within burger_db --
+-- Creates the table "burgers" within burgers_db
 CREATE TABLE burgers (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	burger_name varchar(255) NOT NULL,
