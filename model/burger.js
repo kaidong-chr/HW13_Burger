d5398a6b89fff758b7b8b7bebcccc84c9e10cda4
Kai Dong <dongkai981@gmail.com>
2021-01-06 18:23:02 -0600

    Added selectAll function

--- model/burger.js
+++ model/burger.js
@@ -1,3 +1,15 @@
 // Import orm to create functions that will interact with the database
 let orm = require("../config/orm.js");
 
+// Object that will call orm methods using burger inputs
+let burger = {
+    selectAll: function(cb) {
+        orm.selectAll("burgers", function(res) {
+            cb(res);
+        });
+    },
+
+}
+
+// Export the database functions for the controller - burgers_controller.js
+module.exports = burger
