3932de45e12ac019d5713362b988deaf711339bb
Kai Dong <dongkai981@gmail.com>
2021-01-06 18:08:45 -0600

    Added insertOne method into orm.js

--- config/orm.js
+++ config/orm.js
@@ -2,9 +2,19 @@
 const connection = require("./connection");
 
 let orm = {
-    selectAll: function(tableInput, cb){
-        let queryString = "SELECT * FROM " + tableInput;
-        connection.query(queryString, function(err, res){
+    selectAll: function(table, cb){
+        let queryString = "SELECT * FROM ??";
+        connection.query(queryString, [table], function(err, res){
+            if (err) {
+                throw err;
+            }
+            cb(res);
+        });
+    },
+    
+    insertOne: function(table, col, burger_name, cb){
+        let queryString = "INSERT INTO ?? (??) VALUES (?)";
+        connection.query(queryString, [table, col, burger_name], function(err, res){
             if (err) {
                 throw err;
             }
