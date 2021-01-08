b060de301ba2355fe004664bfb5788877cccf2b1
Kai Dong <dongkai981@gmail.com>
2021-01-08 11:56:48 -0600

    Added root router

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -3,3 +3,21 @@ let express = require("express");
 let burger = require('../models/burger');
 
 let router = express.Router();
+
+// Import the model (burger.js) to use its database functions.
+let burger = require("../models/burger.js");
+
+// Create all our routes and set up logic within those routes where required.
+router.get("/", function(req, res) {
+  burger.selectAll(function(data) {
+    let hbsObject = {
+      burgers: data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
+});
+
+
+// Export routes for server.js to use.
+module.exports = router;
